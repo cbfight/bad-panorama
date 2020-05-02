59e06bb2678a31545e927a2206577ed921c17ce6
wesley <wesleyxwong@gmail.com>
2020-05-02 03:27:56 -0400

    background correction and vignette removal

--- bad_panorama.py
+++ bad_panorama.py
@@ -8,66 +8,70 @@ import statistics
 import numpy as np
 import os
 
+
 #get home
 home = os.path.expanduser("~")
 savepath = os.path.join(home,'Desktop')
 #stitcher
-stitcher = cv2.createStitcher()
+stitcher = cv2.createStitcher(cv2.Stitcher_SCANS)
 
-#file dialogue
+#file dialogue bg-correction
 root = Tk()
-root.filenames = filedialog.askopenfilenames(initialdir="/",title="select images")
+root.filename = filedialog.askopenfilename(initialdir="/",title="select background image")
 root.destroy()
+background = io.imread(root.filename)
+
+#file dialogue inputs
+root = Tk()
+root.filenames = filedialog.askopenfilenames(initialdir="/",title="select stitch images")
+root.destroy()
+
 
 fins = root.tk.splitlist(root.filenames) #list of files
 
 plotDims = math.ceil(math.sqrt(len(fins))) #get grid dimensions
 
-#show single images
+
+background_average_r = np.average(background[:,:,0])
+background_average_g = np.average(background[:,:,1])
+background_average_b = np.average(background[:,:,2])
+
+
+#show cropped single images and make stitch
 fig, ax = plt.subplots()
 i = 1
-for image in fins:
+stitchQueue = []
+for im in fins:
+    image = cv2.imread(im)
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    h, w = image.shape[:2]
+    bg_thresh = 0.1
+    # Make mask of black pixels - mask is True where image is black
+    mBlack = (image[:, :, 0:3] < [background_average_r*bg_thresh,background_average_g*bg_thresh,background_average_b*bg_thresh]).all(2)
+    # Make all pixels matched by mask into transparent ones
+    image[mBlack] = (background_average_r,background_average_g,background_average_b)
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     fig.add_subplot(plotDims,plotDims,i)
     ax.axis('off')
     plt.axis('off')
-    io.imshow(image)
+    io.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
     i+=1
+    stitchQueue.append(image)
 plt.show()
 
-#stitch
-stitchQueue = []
-for image in fins:
-    needs_stitch=cv2.imread(image)
-    stitchQueue.append(needs_stitch)
+    
 
 status, stitched = stitcher.stitch(stitchQueue)
 stitched = cv2.cvtColor(stitched, cv2.COLOR_BGR2RGB)
-stitchednp = np.array(stitched)
+stitchednp = np.array(cv2.cvtColor(stitched,cv2.COLOR_BGR2GRAY),dtype='uint8')
 
-#crop
-rows_w_image = []
-cols_w_image = []
-flat_image = np.add(stitchednp[:,:,0],stitchednp[:,:,1],stitchednp[:,:,2])
-row_counter = 0
-col_counter = 0
-for row in flat_image:
-    if statistics.mean(row) > 5:
-        rows_w_image.append(row_counter)
-    row_counter += 1
 
-for col in np.transpose(flat_image):
-    if statistics.mean(col) > 5:
-        cols_w_image.append(col_counter)
-    col_counter += 1
 
-left_boundary = rows_w_image[0]
-right_boundary = rows_w_image[-1]
-top_boundary = cols_w_image[0]
-bottom_boundary = cols_w_image[-1]
 
-cropped = stitched[left_boundary:right_boundary,top_boundary:bottom_boundary,:]
+#cropped = stitched[left_boundary:right_boundary,top_boundary:bottom_boundary,:]
 
-io.imshow(cropped)
+
+io.imshow(stitched)
 plt.axis('off')
 plt.savefig(str(savepath+'/test.png'),bbox_inches='tight', dpi=1200)
 plt.show()
